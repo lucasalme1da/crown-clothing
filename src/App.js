74b5da89c340fc333a39c095c0c3c854ec6656e1
Lucas Almeida <lucasdealmeida.ss@gmail.com>
2020-06-25 16:39:22 -0300

    Broken the homepage into smaller components, finished styling the menuItem

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 
-import HomePage from './components/HomePage'
+import HomePage from './pages/HomePage'
 
 function App() {
   return (
---
+++ src/pages/HomePage/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './styles.scss'
+
+import Directory from '../../components/Directory';
+
+const HomePage = () => (
+  <div className="homepage">
+    <Directory />
+  </div>
+);
+
+export default HomePage;
