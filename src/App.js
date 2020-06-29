7d15e4da81665dafc209985161b346d4ff528e6f
Lucas Almeida <lucasdealmeida.ss@gmail.com>
2020-06-29 16:54:14 -0300

    Finishing sign in handleSubmit method

--- src/App.js
+++ src/App.js
@@ -34,6 +34,7 @@ class App extends React.Component {
             }
           })
         })
+
       } else {
         this.setState({ currentUser: userAuth })
       }
--- src/components/SignIn/index.jsx
+++ src/components/SignIn/index.jsx
@@ -4,7 +4,7 @@ import './styles.scss'
 import FormInput from '../FormInput';
 import CustomButton from '../CustomButton';
 
-import { signInWithGoogle } from '../../firebase/firebase.utils'
+import { auth, signInWithGoogle } from '../../firebase/firebase.utils'
 
 class SignIn extends React.Component {
   constructor(props) {
@@ -24,13 +24,21 @@ class SignIn extends React.Component {
     })
   }
 
-  handleSubmit = event => {
+  handleSubmit = async event => {
     event.preventDefault();
 
-    this.setState({
-      email: '',
-      password: '',
-    })
+    const { email, password } = this.state;
+
+    try {
+      await auth.signInWithEmailAndPassword(email, password);
+      this.setState({
+        email: '',
+        password: '',
+      })
+    } catch (error) {
+      console.log(error);
+    }
+
   }
 
   render() {
